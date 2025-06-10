07b225ccebcc71e4e82efcbb48ee74af0d1f10ae
tenshells <shelton.dany@gmail.com>
2025-06-10 21:18:26 +0530

    test local ollama server response

--- local_llm_comms.py
+++ local_llm_comms.py
@@ -1,2 +1,18 @@
 from ollama import chat
 
+from ollama import ChatResponse
+
+
+
+response: ChatResponse = chat(model='llama3.2:latest', messages=[
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?, answer in 10 words',
+  },
+])
+# or access fields directly from the response object
+print(response.message.content)
+
+import ollama
+all_available_models = ollama.list()
+print(all_available_models)
