d65cb91d4e0e866d9b468c0859f37666e5284cf4
tenshells <shelton.dany@gmail.com>
2025-06-16 16:04:50 +0530

    fetch pr details

--- poc/github/fetch_pr.py
+++ poc/github/fetch_pr.py
@@ -10,11 +10,21 @@ auth = Auth.Token(GITHUB_TOKEN)
 
 # First create a Github instance:
 g = Github(auth=auth)
-
-prs = g.get_repo("tenshells/pr-opper").get_pulls()
+username = g.get_user().login
+prs = g.get_repo(f"{username}/pr-opper").get_pulls()
 
 for pr in prs:
+    print(pr)
     print(pr.title)
-
+    print(pr.body)
+    print(pr.state)
+    print(pr.user.login)
+    print(pr.created_at, pr.updated_at, pr.closed_at, pr.merged_at)
+    print(pr.base.ref)
+    print()
+    for commit in pr.get_commits():
+        print(commit, commit.commit, commit.commit.message)
+    for file in pr.get_files():
+        print(file.filename, file.status, file.patch)
 # To close connections after use
 g.close()
